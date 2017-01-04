c37badd9c0d5fa55ffce7100de8f9bc8f6cbb858
AnnaSu <newstory0113@gmail.com>
2017-01-04 21:41:23 +0800

    顯示待辦事項清單

--- src/main.js
+++ src/main.js
@@ -8,9 +8,26 @@
     .then(res => res.json())
     .then(json => {
         todoList = todoList.concat(json);
+        render(todoList);
     })
     .catch(err => {
         console.log(err);
     })
+
+    // 顯示待辦事項清單 - render 所有 todoList item
+    function renderTodoList (todoList) {
+        const html = todoList.map((item, index) => `<li class="list">
+                <a class="${item.isComplete ? 'finish' : 'unfinished'}" data-id=${item.id}></a>
+                <p class="desc" data-id=${item.id}>
+                    ${item.desc}
+                </p>
+                <a class="del" data-id=${item.id}></a>
+            </li>`).join('')
+        todoListDOM.innerHTML = html;
+    }
+
+    function render (todoList) {
+        renderTodoList(todoList);
+    }
 
 }(window, document));
