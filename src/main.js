a2adc6b8e1073b15ca9f0271891f781d15ebb263
AnnaSu <newstory0113@gmail.com>
2017-01-04 21:39:21 +0800

    取得待辦事項清單（GET）

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 (function (window, document) {
 
     const todoListDOM = document.getElementById('todoList');
+    let todoList = [];
+
+    // 取得待辦事項清單（GET）
+    fetch('http://localhost:3000/todolist')
+    .then(res => res.json())
+    .then(json => {
+        todoList = todoList.concat(json);
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
 }(window, document));
